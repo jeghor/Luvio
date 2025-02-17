12d56a269ee1ba2854a4b303a259ca8e3d8b7012
Egor Shklyarevsky <egor39546@gmail.com>
2025-02-17 19:02:42 +0300

    - add base custom ui components and icons. Add Theme for app
    Also add dependencies for compose

---
+++ sources/ui_core/src/main/java/com/example/ui_core/custom_views/TextButton.kt
@@ -0,0 +1,56 @@
+package com.example.ui_core.custom_views
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ui_core.theme.AppTheme
+
+@Composable
+fun TextButton(
+    modifier: Modifier,
+    @DrawableRes startIcon: Int,
+    text: String
+) {
+    Row(
+        modifier = modifier
+            .height(32.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painterResource(startIcon),
+            modifier = Modifier.padding(end = 12.dp),
+            contentDescription = "Start icon",
+        )
+        Text(
+            modifier = Modifier.weight(1F),
+            text = text,
+            style = AppTheme.typography.bodySmall
+        )
+        Icon(
+            painterResource(com.example.ui_atoms.R.drawable.ic_arrow),
+            modifier = Modifier
+                .padding(start = 12.dp),
+            contentDescription = "End icon"
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TextButtonPreview() {
+    Surface {
+        TextButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            startIcon = com.example.ui_atoms.R.drawable.ic_camera,
+            text = "Memoriesqwdqwdqdwdqdwdq"
+        )
+    }
+}
---
+++ sources/ui_core/src/main/java/com/example/ui_core/theme/AppColors.kt
@@ -0,0 +1,40 @@
+package com.example.ui_core.theme
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val primary = Color(0xFFFF5E78)
+    val secondary = Color(0xFFF6F6F6)
+    val background = Color(0xFFFFFFFF)
+    val transparent = Color(0x00000000)
+    val textPrimary = Color(0xFF333333)
+    val textHint = Color(0xFF888888)
+}
+
+interface ColorPalette {
+    val primary: Color
+    val secondary: Color
+    val background: Color
+    val transparent: Color
+    val textPrimary: Color
+    val textHint: Color
+
+    val materialColors: ColorScheme
+}
+
+fun lightColorPalette(): ColorPalette = object : ColorPalette {
+    override val primary: Color = AppColors.primary
+    override val secondary: Color = AppColors.secondary
+    override val background: Color = AppColors.background
+    override val transparent: Color = AppColors.transparent
+    override val textPrimary: Color = AppColors.textPrimary
+    override val textHint: Color = AppColors.textHint
+
+    override val materialColors: ColorScheme = lightColorScheme(
+        primary = primary,
+        surface = textPrimary,
+        onSurface = background
+    )
+}
