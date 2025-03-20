584761a0b6d7bbe1b8bb82fa3c8a1040b79e6ab1
Egor Shklyarevsky <egor39546@gmail.com>
2025-03-20 18:41:33 +0300

    - remove BaseUrl from repository

---
+++ sources/network/api/src/main/java/com/luvio/api/BaseUrl.kt
@@ -0,0 +1,6 @@
+package com.luvio.api
+
+object BaseUrl {
+
+    const val URL = "https://routeapp.catscode.ru"
+}
--- sources/network/api/src/main/java/com/luvio/api/NetworkRoutes.kt
+++ sources/network/api/src/main/java/com/luvio/api/NetworkRoutes.kt
@@ -2,13 +2,11 @@ package com.luvio.api
 
 object NetworkRoutes {
 
-    private const val BASE_URL = "https://routeapp.catscode.ru"
-
     object Auth {
         private const val AUTH_PATH = "/auth"
 
-        const val REGISTER = "${BASE_URL+ AUTH_PATH}/register"
-        const val VERIFY_EMAIL = "${BASE_URL+ AUTH_PATH}/auth/verify-email"
-        const val LOGIN = "${BASE_URL+ AUTH_PATH}/login"
+        const val REGISTER = "${BaseUrl.URL + AUTH_PATH}/register"
+        const val VERIFY_EMAIL = "${BaseUrl.URL + AUTH_PATH}/auth/verify-email"
+        const val LOGIN = "${BaseUrl.URL+ AUTH_PATH}/login"
     }
 }
